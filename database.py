8ed6b8177af5f1dc3d5d2804433cff613b07370b
sk1746717-hub <sk1746717@gmail.com>
2026-06-23 17:09:44 +0530

    Use environment variable for database

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql+psycopg://postgres:postgre143@localhost:5432/product_browser"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
